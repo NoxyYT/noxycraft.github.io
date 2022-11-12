9550670deeec4c40b68ec425adc125c56076f56f
NoxyYT <noxycraftyt@gmail.com>
2022-11-12 20:09:19 +0200

    f
    f

---
+++ lib/configbg.php
@@ -0,0 +1,11 @@
+﻿<?php 
+	$nameDB = "u-world";
+	$nameSERVER = "localhost";
+	$nameUSER = "root";
+	$passUSER = "56eu-world.sudsiuKbb779";
+
+	$conDB = mysqli_connect($nameSERVER, $nameUSER, $passUSER, $nameDB) 
+	or die("ERROR".mysqli_error($nameDB));
+
+	mysqli_query($conDB, "SET NAMES utf8");
+?>
